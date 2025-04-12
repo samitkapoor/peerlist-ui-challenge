f38e26ffec8ca36dcb8ed95012500bb7ae253586
samitkapoor <samitkapoor77@gmail.com>
2025-04-12 13:17:53 +0530

    functionality is completed

---
+++ src/challenges/FluidMenuPage.tsx
@@ -0,0 +1,11 @@
+import FluidMenu from '../components/FluidMenu';
+
+const FluidMenuPage = () => {
+  return (
+    <div className="h-screen w-screen bg-white/80 p-5 relative">
+      <FluidMenu />
+    </div>
+  );
+};
+
+export default FluidMenuPage;
---
+++ src/components/FluidMenu.tsx
@@ -0,0 +1,115 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { Home, Mail, Menu, Settings, User, X } from 'lucide-react';
+import { useState } from 'react';
+
+interface Option {
+  name: string;
+  icon: React.ReactNode;
+}
+
+const options: Option[] = [
+  {
+    name: 'Home',
+    icon: <Home />
+  },
+  {
+    name: 'Message',
+    icon: <Mail />
+  },
+  {
+    name: 'Profile',
+    icon: <User />
+  },
+  {
+    name: 'Settings',
+    icon: <Settings />
+  }
+];
+
+const MenuButton = ({
+  option,
+  onClick,
+  className
+}: {
+  option: Option;
+  onClick?: () => void;
+  className?: string;
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`h-[52px] w-[52px] p-3 !rounded-full !outline-none !border-none !bg-white flex items-center justify-center ${className}`}
+    >
+      {option.icon}
+    </button>
+  );
+};
+
+const FluidMenu = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <div className="text-black flex flex-col gap-0 relative">
+      <AnimatePresence>
+        {!isOpen && (
+          <motion.div
+            initial={{ filter: 'blur(10px)' }}
+            animate={{ filter: 'blur(0px)' }}
+            exit={{ filter: 'blur(10px)' }}
+            className="z-10"
+          >
+            <MenuButton
+              onClick={() => setIsOpen(!isOpen)}
+              option={{
+                name: 'Menu',
+                icon: <Menu />
+              }}
+            />
+          </motion.div>
+        )}
+        {isOpen && (
+          <motion.div
+            initial={{ filter: 'blur(10px)' }}
+            animate={{ filter: 'blur(0px)' }}
+            exit={{ filter: 'blur(10px)' }}
+            className="z-10"
+          >
+            <MenuButton
+              onClick={() => setIsOpen(!isOpen)}
+              option={{
+                name: 'X',
+                icon: <X />
+              }}
+              className="z-10"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {isOpen &&
+          options.map((option, index) => {
+            const calculatePosition = 48 * (index + 1);
+
+            return (
+              <motion.div
+                key={index}
+                animate={{
+                  top: calculatePosition + 'px',
+                  opacity: 1
+                }}
+                exit={{
+                  top: '0px',
+                  opacity: 0
+                }}
+                className="h-[52px] w-[52px] absolute "
+              >
+                <MenuButton option={option} key={option.name} />
+              </motion.div>
+            );
+          })}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default FluidMenu;
