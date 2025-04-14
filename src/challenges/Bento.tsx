ef8096291ca88d8e60f15d2423a8bf495c592688
samitkapoor <samitkapoor77@gmail.com>
2025-04-14 23:42:22 +0530

    responsiveness

--- src/challenges/Bento.tsx
+++ src/challenges/Bento.tsx
@@ -10,7 +10,7 @@ const Bento = () => {
       id: 1,
       title: 'Day 1 - Fluid Menu',
       children: <FluidMenuPage />,
-      className: 'col-span-3 row-span-8',
+      className: 'col-span-1 md:col-span-3 row-span-8',
       href: 'https://peerlist.io/challenges/ui-animation-challenge?utm_source=left-panel',
       background: (
         <div className="absolute inset-0">
@@ -30,7 +30,7 @@ const Bento = () => {
       id: 2,
       title: 'Day 2 - Dynamic Status Indicator',
       children: <DynamicStatusIndicator />,
-      className: 'col-span-5 row-span-1',
+      className: 'col-span-1 md:col-span-5 row-span-1',
       href: 'https://peerlist.io/challenges/ui-animation-challenge?utm_source=left-panel',
       background: (
         <div className="absolute inset-0">
@@ -50,13 +50,13 @@ const Bento = () => {
       title: 'Day 3 - Todo List',
       href: 'https://peerlist.io/challenges/ui-animation-challenge?utm_source=left-panel',
       children: <TodoList />,
-      className: 'col-span-4 row-span-2'
+      className: 'col-span-1 md:col-span-4 row-span-2'
     },
     {
       id: 4,
       title: 'Coming Soon',
       children: <LockedItem />,
-      className: 'col-span-4 row-span-7',
+      className: 'col-span-1 md:col-span-4 row-span-7',
       background: (
         <div className="absolute inset-0">
           <div
@@ -74,7 +74,7 @@ const Bento = () => {
       id: 5,
       title: 'Coming Soon',
       children: <LockedItem />,
-      className: 'col-span-5 row-span-6',
+      className: 'col-span-1 md:col-span-5 row-span-6',
       background: (
         <div className="absolute inset-0">
           <div
@@ -91,50 +91,53 @@ const Bento = () => {
   ];
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center flex-col select-none bg-[#eeeeee]">
-      <div
-        style={{
-          background: 'radial-gradient(circle at left, #FFFFFF, transparent 80%)'
-        }}
-        className="absolute top-0 left-0 h-full w-[700px]"
-      ></div>
-      <div
-        style={{
-          background: 'radial-gradient(circle at right, #FFFFFF, transparent 80%)'
-        }}
-        className="absolute top-0 right-0 h-full w-[700px]"
-      ></div>
-      <div className="flex items-center text-7xl text-black font-extrabold py-2 z-10">
-        <div
-          style={{
-            background: 'linear-gradient(135deg, #ffffff, #f8fdf8)',
-            boxShadow: 'inset 0 0 15px rgba(0, 128, 0, 0.35), 0 0 50px rgba(0, 128, 0, 0.25)'
-          }}
-          className="h-20 w-20 rounded-3xl border border-green-400/30 flex items-center justify-center p-2"
-        >
-          <img src="/peerlist.svg" className="w-full h-full" />
+    <div className="min-h-screen w-screen flex items-center justify-center flex-col select-none bg-[#eeeeee] p-5 md:p-10">
+      <div className="flex flex-row items-center text-3xl md:text-7xl text-black font-extrabold py-2 z-10">
+        <div className="flex items-center">
+          <div
+            style={{
+              background: 'linear-gradient(135deg, #ffffff, #f8fdf8)',
+              boxShadow: 'inset 0 0 15px rgba(0, 128, 0, 0.35), 0 0 50px rgba(0, 128, 0, 0.25)'
+            }}
+            className="h-14 w-14 md:h-20 md:w-20 rounded-3xl border border-green-400/30 flex items-center justify-center p-2"
+          >
+            <img src="/peerlist.svg" className="w-full h-full" />
+          </div>
+          <p className="text-black/80 font-bold">eerlist</p>
         </div>
-        <p className="text-black/80 font-bold">
-          eerlist&nbsp;<span className="text-green-600 font-normal">x</span>&nbsp;
-        </p>
 
-        <div
-          style={{
-            background: 'linear-gradient(135deg, #ffffff, #f8fdf8)',
-            boxShadow: 'inset 0 0 15px rgba(0, 128, 0, 0.35), 0 0 50px rgba(0, 128, 0, 0.25)'
-          }}
-          className="h-20 w-20 rounded-3xl border border-green-400/30 flex items-center justify-center p-2"
-        >
-          <img src="/aceternity.png" className="w-full h-full object-contain" />
+        <span className="text-green-600 font-normal">&nbsp;x&nbsp;</span>
+
+        <div className="flex items-center">
+          <div
+            style={{
+              background: 'linear-gradient(135deg, #ffffff, #f8fdf8)',
+              boxShadow: 'inset 0 0 15px rgba(0, 128, 0, 0.35), 0 0 50px rgba(0, 128, 0, 0.25)'
+            }}
+            className="h-14 w-14 md:h-20 md:w-20 rounded-3xl border border-green-400/30 flex items-center justify-center p-2"
+          >
+            <img src="/aceternity.png" className="w-full h-full object-contain" />
+          </div>
+          <p className="text-black/80 font-bold">ceternity</p>
         </div>
-        <p className="text-black/80 font-bold">ceternity</p>
       </div>
-      <div className="grid grid-cols-12 gap-5 mt-12 z-10 max-w-5xl">
+
+      <p className="text-sm text-black/80 mt-4 text-center">
+        Below are my submissions for this UI animation challenge.{' '}
+        <a
+          href="https://peerlist.io/challenges/ui-animation-challenge?utm_source=left-panel"
+          className="!underline"
+        >
+          Click to know more.
+        </a>
+      </p>
+
+      <div className="grid md:grid-cols-12 gap-5 mt-12 z-10 max-w-5xl">
         {components.map((component) => {
           return (
             <div
               key={component.id}
-              className={`${component.className} min-h-[200px] rounded-xl overflow-hidden relative transition-all duration-300 hover:translate-y-[-4px] border-[1px] border-green-500/30 group hover:shadow-lg`}
+              className={`${component.className} min-h-[300px] md:min-h-[200px] rounded-xl overflow-hidden relative transition-all duration-300 hover:translate-y-[-4px] border-[1px] border-green-500/30 group hover:shadow-lg`}
             >
               <div className="pointer-events-none opacity-70 transition-all duration-300">
                 {component.background}
@@ -158,6 +161,12 @@ const Bento = () => {
           );
         })}
       </div>
+      <p className="text-sm text-black/80 mt-12">
+        Created with ❤️ by{' '}
+        <a href="https://samitkapoor.com" className="!underline !text-green-600">
+          Samit Kapoor
+        </a>
+      </p>
     </div>
   );
 };
